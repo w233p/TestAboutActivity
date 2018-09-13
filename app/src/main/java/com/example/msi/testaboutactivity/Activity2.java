1d1e0629dcb1ea3e0f7fb8666dfdf4c000d2b768
w233p <43175275+w233p@users.noreply.github.com>
2018-09-13 21:53:07 +0800

    onActivityResult
    添加了onActivityResult的用法

---
+++ app/src/main/java/com/example/msi/testaboutactivity/Activity2.java
@@ -0,0 +1,35 @@
+package com.example.msi.testaboutactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Activity2 extends AppCompatActivity {
+
+    EditText mNameEt = null;
+    Button mBackBt = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+
+        mNameEt = (EditText)findViewById(R.id.nameEt);
+        mBackBt = (Button)findViewById(R.id.backBt);
+        mBackBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                Bundle bundle = new Bundle();
+                bundle.putString("name",mNameEt.getText().toString());
+                intent.putExtras(bundle);
+                Activity2.this.setResult(1,intent);
+                Activity2.this.finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/msi/testaboutactivity/MainActivity.java
+++ app/src/main/java/com/example/msi/testaboutactivity/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.msi.testaboutactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv1 = null;
+    TextView mTv1 = null;
+    Button mBt1 = null;
 
     @Override
     protected void onStart() {
@@ -46,33 +49,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("onCreate", "onCreate");
 
-        tv1 = (TextView) findViewById(R.id.tv1);
-        if(savedInstanceState!=null){
-            tv1.setText(savedInstanceState.getString("tv1_text","未点击"));
-        }else {
-            tv1.setText("未点击");
+        mTv1 = (TextView) findViewById(R.id.tv1);
+        if (savedInstanceState != null) {
+            mTv1.setText(savedInstanceState.getString("tv1_text", "未点击"));
+        } else {
+            mTv1.setText("未点击");
         }
-        tv1.setOnClickListener(new View.OnClickListener() {
+        mTv1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tv1.setText("点击");
+                mTv1.setText("点击");
+            }
+        });
+
+        mBt1 = (Button) findViewById(R.id.bt1);
+        mBt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Activity2.class);
+                startActivityForResult(intent, 1);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("tag:", "onActivityResult");
+        if (requestCode == 1) {
+            if (resultCode == 1) {
+                Bundle bundle =data.getExtras();
+                if(bundle!=null){
+                    Log.d("tag:", "bundle get");
+                    String msg= bundle.getString("name");
+                    mTv1.setText(msg);
+                }
+            }
+        }
+    }
+
     /*通过重写onSaveInstanceState方法保存数据
-    * 该方法适用于简单临时的数据
-    * 自定义对象数据或者socket等较大数据使用setRetainInstance(boolean)来保存,此处留坑*/
+     * 该方法适用于简单临时的数据
+     * 自定义对象数据或者socket等较大数据使用setRetainInstance(boolean)来保存,
+     * 一般使用该方法保存Fragment*/
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        Log.d("tag:","onSaveInstanceState");
-        outState.putString("tv1_text",tv1.getText().toString());
+        Log.d("tag:", "onSaveInstanceState");
+        outState.putString("tv1_text", mTv1.getText().toString());
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        Log.d("tag:","onRestoreInstanceState");
+        Log.d("tag:", "onRestoreInstanceState");
     }
+
+
 }
